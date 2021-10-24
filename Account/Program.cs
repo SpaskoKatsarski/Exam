6152d899aea99a9d9e013921271b40f5e41cd002
Spasko Katsarski <patsokatsarski1@gmail.com>
2021-10-25 00:48:07 +0300

    Updated

---
+++ Account/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+namespace Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User, please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("User, please enter your password:");
+            string password = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Thanks for logging in!");
+            Console.WriteLine();
+
+            Thread.Sleep(1000);
+            bool flag = false;
+
+            Console.WriteLine("Do you wish to stay logged in?");
+
+            string option = Console.ReadLine();
+
+            if (option == "Yes" || option == "yes")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enjoy exploring!");
+            }
+            else if (option == "No" || option == "no")
+            {
+
+                Console.WriteLine();
+                Console.WriteLine("See you later!");
+                Console.WriteLine();
+                flag = true;
+            }
+
+            if (flag)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Hi again! Please enter your passowrd from before:");
+                int counter = 5;
+                while (counter > 0)
+                {
+                    counter--;
+                    string input = Console.ReadLine();
+                    Console.WriteLine();
+                    if (input == password)
+                    {
+                        Console.WriteLine($"Welcome {name}!");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Wrong password!\nYou have {counter} more tries.");
+                        Console.WriteLine();
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("This user has attempted too many invalid passwords. Try again later.");
+        }
+    }
+}
---
+++ Test2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            double num2 = 0;
+
+            Console.WriteLine(num1 / num2);
+        }
+    }
+}
